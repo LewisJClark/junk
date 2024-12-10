d309e6586948d2b52e85594a99a0401a978c0366
Lewis Clark <clarkjacklewis@gmail.com>
2024-12-10 19:09:33 +0000

    Added sprites for different button states to spriteButton

--- ui/uiSpriteButton.lua
+++ ui/uiSpriteButton.lua
@@ -9,16 +9,25 @@ local uiSpriteButton = class("uiSpriteButton", uiButton)
 function uiSpriteButton:initialize(id, config)
    uiButton.initialize(self, id, config)
    self.sprite = config.sprite and game.assets:createSprite(config.sprite) or nil
+   self.focused_sprite = config.sprite and game.assets:createSprite(config.focused_sprite) or nil
+   self.pressed_sprite = config.sprite and game.assets:createSprite(config.pressed_sprite) or nil
 end
 
 function uiSpriteButton:draw()
    local colour = self.colour
-   if self.has_focus then colour = self.focused_colour end
-   if self.pressed then colour = self.pressed_colour end
+   local sprite = self.sprite
+   if self.has_focus then 
+      colour = self.focused_colour
+      sprite = self.focused_sprite
+   end
+   if self.pressed then
+      colour = self.pressed_colour
+      sprite = self.pressed_sprite
+   end
    love.graphics.setColor(colour.r, colour.g, colour.b, colour.a)
    love.graphics.rectangle("fill", self.global_x, self.global_y, self.width, self.height)
    love.graphics.setColor(1, 1, 1, 1)
-   self.sprite:draw(self.global_x, self.global_y)
+   sprite:draw(self.global_x, self.global_y)
 end
 
 return uiSpriteButton
